946c48b8fa8eb3bcdea7a0651803aca53af9db37
Laust <laust.bonnesen@mail.com>
2021-07-23 22:48:31 +0200

    get() is edited

--- src/Liszt.java
+++ src/Liszt.java
@@ -9,6 +9,13 @@ import java.util.stream.Stream;
 
     Implements same objects as ArrayList at this moment
 
+    Differences array wise are:
+
+    DEFAULT_CAPACITY is 1
+    Index of array starts at 1 instead of 0
+    Parameter inputs for initiating values are as E[]
+    Differently organized with {} and other that makes it easier to read, but takes more space
+
     Has attributes of an array of E and as well an Map, which needs to be determined whether it's a hashmap or treemap.
     That way an object of E can quickly be returned and the whole array can be looped thru easily
 
@@ -17,45 +24,46 @@ import java.util.stream.Stream;
 // Author Laust Eberhardt Bonnesen
 public class Liszt<E> extends AbstractList<E> implements List<E>, RandomAccess, Cloneable, java.io.Serializable {
 
-    private Map<String,E> map;
+    private Map<String,E> mapData;
 
     private int size;
 
-    private String defaultKeySymbol = "Д";
-
     private transient Object[] elementData;
     private static final Object[] EMPTY_ELEMENTDATA = {};
-    private static final int DEFAULT_CAPACITY = 10;
+    private static final int DEFAULT_CAPACITY = 1;
     private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE;
 
+    // Default constructor, that automatically makes the mapData an HashMap and the size 1
     public Liszt() {
         this(true,1);
     }
 
+    // An constructor that will determine size and whether it's an HashMap or not
     public Liszt(boolean isHashMap, int size) {
         super();
         this.size = size;
         if (isHashMap) {
-            map = new HashMap<>();
+            mapData = new HashMap<>();
         }
         else {
-            map = new TreeMap<>();
+            mapData = new TreeMap<>();
         }
-        this.elementData = new Object[this.size];
+        this.elementData = new Object[this.size+1];
     }
 
-    public Liszt(boolean isHashMap, E[] firstValues) {
+    // Alternatively this is for already existing values when initialyzing
+    public Liszt(boolean isHashMap, E[] initializingValues) {
         super();
         if (isHashMap) {
-            map = new HashMap<>();
+            mapData = new HashMap<>();
         }
         else {
-            map = new TreeMap<>();
+            mapData = new TreeMap<>();
         }
-        this.elementData = addArray(firstValues);
+        this.elementData = addArray(initializingValues);
     }
 
-    //Both these two next methods returns the size of the array, but allows the command length that are of an array[]
+    // Both these two next methods returns the size of the array, but allows the command length that are of an array[]
     @Override
     public int size() {
         return size;
@@ -76,10 +84,10 @@ public class Liszt<E> extends AbstractList<E> implements List<E>, RandomAccess, 
     }
 
     public boolean contains(String key) {
-        return map.containsKey(key);
+        return mapData.containsKey(key);
     }
 
-    //To insure that an object is registered in map
+    // To insure that an object is registered in map
     public boolean isObjectInMap(String key, Object object) {
         if (contains(key) && contains(object)) {
             return true;
@@ -136,17 +144,17 @@ public class Liszt<E> extends AbstractList<E> implements List<E>, RandomAccess, 
     public boolean add(E element) {
         ensureCapacityInternal(size + 1);  // Increments modCount!!
         elementData[size++] = element;
-        map.put(defaultKeySymbol+size,element);
+        mapData.put(element.toString(),element);
         return true;
     }
 
-    //Adds an entire array of E at the end of the current array
+    // Adds an entire array of E at the end of the current array
     public E[] addArray(E[] objects) {
         int amountOfIndexs = size + objects.length;
         ensureCapacityInternal(amountOfIndexs);  // Increments modCount!!
         for (int i = 0; i < amountOfIndexs;i++) {
             elementData[size++] = objects;
-            map.put(defaultKeySymbol+size,objects[i]);
+            mapData.put(objects[i].toString(),objects[i]);
         }
         return (E[]) elementData;
     }
@@ -156,7 +164,7 @@ public class Liszt<E> extends AbstractList<E> implements List<E>, RandomAccess, 
         ensureCapacityInternal(amountOfIndexs);  // Increments modCount!!
         for (int i = 0; i < amountOfIndexs;i++) {
             elementData[size++] = objects;
-            map.put(keys[i],objects[i]);
+            mapData.put(keys[i],objects[i]);
         }
         return (E[]) elementData;
     }
@@ -204,12 +212,6 @@ public class Liszt<E> extends AbstractList<E> implements List<E>, RandomAccess, 
                 MAX_ARRAY_SIZE;
     }
 
-
-    @Override
-    public boolean remove(Object o) {
-        return false;
-    }
-
     @Override
     public boolean containsAll(Collection<?> c) {
         return false;
@@ -253,12 +255,33 @@ public class Liszt<E> extends AbstractList<E> implements List<E>, RandomAccess, 
     @Override
     public void clear() {
         elementData = (E[]) new Object[0];
-        map.clear();
+        mapData.clear();
     }
 
+    // Returns E index starting with 1 instead of 0
     @Override
     public E get(int index) {
-        return null;
+        rangeCheck(index+1);
+
+        if (index < 1) {
+            throw new InputMismatchException("Index size starts at 1...");
+        }
+
+        return (E) elementData[index+1];
+    }
+
+    private void rangeCheck(int index) {
+        if (index >= size) {
+            throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
+        }
+    }
+
+    private String outOfBoundsMsg(int index) {
+        return "Index: "+index+", Size: "+size;
+    }
+
+    public E get(String key) {
+        return mapData.get(key);
     }
 
     @Override
@@ -273,6 +296,23 @@ public class Liszt<E> extends AbstractList<E> implements List<E>, RandomAccess, 
 
     //Removes the index in the array of the parameter input, and returns the E of that is removed. It sorts the array as well
     @Override
+    public boolean remove(Object object) {
+        Object[] newList = new Object[size-1];
+        boolean objectIsRemoved = false;
+
+        for (int i = 0; i < size-1;i++) {
+            if (objectIsRemoved) {
+                newList[i] = elementData[i+1];
+            }
+            else if (elementData.equals(object)) {
+                mapData.remove(elementData[i].toString());
+            }
+        }
+        elementData = newList;
+        return false;
+    }
+
+    @Override
     public E remove(int index) {
         Object[] newList = new Object[size-1];
         Object elementOfDelete = new Object();
@@ -286,6 +326,7 @@ public class Liszt<E> extends AbstractList<E> implements List<E>, RandomAccess, 
             }
             else {
                 elementOfDelete = elementData[i];
+                mapData.remove(elementData[i].toString());
             }
         }
         elementData = newList;
@@ -309,7 +350,7 @@ public class Liszt<E> extends AbstractList<E> implements List<E>, RandomAccess, 
             }
         }
         elementData = newList;
-        map.remove(key);
+        mapData.remove(key);
 
         return (E)elementOfDelete;
     }
@@ -335,7 +376,7 @@ public class Liszt<E> extends AbstractList<E> implements List<E>, RandomAccess, 
         elementData = newList;
 
         for (int i = 0; i < keys.length;i++) {
-            map.remove(keys[i]);
+            mapData.remove(keys[i]);
         }
 
         return (E[]) elementsToDelete;
@@ -344,9 +385,16 @@ public class Liszt<E> extends AbstractList<E> implements List<E>, RandomAccess, 
     //Runs through the array, and if the object is identical the to an index, it returns an int of that object's index
     @Override
     public int indexOf(Object object) {
-        for (int i = 0; i < size(); i++) {
-            if (object.equals(elementData[i])) {
-                return i;
+        if (object == null) {
+            for (int i = 0; i < size; i++)
+                if (elementData[i]==null)
+                    return i;
+        }
+        else {
+            for (int i = 0; i < size(); i++) {
+                if (object.equals(elementData[i])) {
+                    return i;
+                }
             }
         }
         return -1;
